2cbd3d57d34eb1368fec61e42cfd668cdce7c736
Fiorela Azahuanche <91639259+fio-azahuanche@users.noreply.github.com>
2022-01-21 18:25:11 -0500

    Update and rename Bienvenido2.java to bienvenido2.java

--- semana1/bienvenido2.java
+++ semana1/bienvenido2.java
@@ -1,11 +1,11 @@
-//Bienvenido2.java
+//bienvenido2.java
 //Imprimir varias lineas en un cuadro de dialogo.
 
-public class Bienvenido2 {
+public class bienvenido2 {
 	//el metodo main empieza la ejecucion de la aplicacion de java
 	public static void main(String args[])
 	{	
 		System.out.printf("%s\n%s\n","Bienvenido a","la programacion en java!");
 		
 	}//fin del metodo main
-}//fin de la clase Bienvenido2
+}//fin de la clase bienvenido2
